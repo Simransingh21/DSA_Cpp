4acc995b4502566e035e8b823befe83d20b5e87e
Simran Singh <simransinghh2120@gmail.com>
2026-03-27 00:10:41 +0530

    Added print number n to 1 and square pattern using for loop

--- LOOPS/Square_pattern_using_for_loop.cpp
+++ LOOPS/Square_pattern_using_for_loop.cpp
@@ -1,19 +1,17 @@
 // Print the square pattern using for loops
+//       *  *  *  *
+//       *  *  *  *
+//       *  *  *  *
+//       *  *  *  * 
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n;
-    cout<<"Enter your n: ";
-    cin>>n;
-
-    //Using for loop
-    
-    for (int i=1; i<=n; i++) {
-    cout << i << " ";
+    for (int i = 1; i <= 4; i++)
+    {
+        cout << " *  *  *  * " << endl;
     }
-    cout << endl;
-
     return 0;
 }
+
--- LOOPS/print_number_n_to_1_for_loop.cpp
+++ LOOPS/print_number_n_to_1_for_loop.cpp
@@ -1,19 +1,17 @@
 // Print numbers n to 1 using for loop
+// 1) initialize => i = n
+// 2) condition => i >= 1
+// 3) update by decreasing by 1, i--
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n;
-    cout<<"Enter your n: ";
-    cin>>n;
-
-    //Using for loop
-    
-    for (int i=1; i<=n; i++) {
-    cout << i << " ";
-    }
-    cout << endl;
-
-    return 0;
+    int n = 12;
+for (int i = n; i >= 1; i--)
+{
+    cout<< i << " ";
+}
+cout << endl;
+return 0;
 }
