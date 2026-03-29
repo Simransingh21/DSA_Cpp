d0f4cf3314785d80c9aef4b4e0c627789abd84a6
Simran Singh <simransinghh2120@gmail.com>
2026-03-29 23:51:15 +0530

    hollow rectangle pattern program

---
+++ PATTERNS/hollow_rectangle.cpp
@@ -0,0 +1,31 @@
+// Print hollow rectangle pattern
+//  * * * * *
+//  *       *
+//  *       *
+//  * * * * *
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "*";      // First star
+
+        for (int j = 1; j <= n - 1; j++)
+        {
+            if (i == 1 || i == n)
+            {
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+            
+        }
+        cout << "*" << endl;      //Last star
+    }
+    return 0;
+}
