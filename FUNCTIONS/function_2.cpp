341622c25b1be6ac4eaccc65815b113a8bc43cc0
Simran Singh <simransinghh2120@gmail.com>
2026-04-06 01:39:52 +0530

    print if a number is odd or even program

---
+++ FUNCTIONS/function_2.cpp
@@ -0,0 +1,17 @@
+// Waf to print if a number is odd or even
+
+#include <iostream>
+using namespace std;
+
+bool isEven(int n) {
+    if (n % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    cout<< isEven(20) << endl;
+    return 0;
+}
