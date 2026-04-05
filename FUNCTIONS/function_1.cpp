54197d5a5c268fcb0d41c8260a263e0a2cd71cd6
Simran Singh <simransinghh2120@gmail.com>
2026-04-06 00:34:14 +0530

    the product of 2 numbers - a & b program

---
+++ FUNCTIONS/function_1.cpp
@@ -0,0 +1,13 @@
+// Waf to find the product of 2 numbers - a & b
+
+#include <iostream>
+using namespace std;
+
+int prod(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    cout<< prod(10,20) << endl;
+    return 0;
+}
